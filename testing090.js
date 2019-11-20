e234cb279542e4075809d348122b3defa8f170d2
Eugene <jurgen.porubenger@gmail.com>
2019-11-20 17:32:43 +0200

    final

--- testing090.js
+++ testing090.js
@@ -1,9 +1,23 @@
-function Robot(){
-    this.__proto__ = Robot.prototype;
-    this.legs=2;
-    return this;
-}
-// function Vehicle(){
+// function Robot(){
+//     this.__proto__ = Robot.prototype;
+//     this.legs=2;
+//     return this;
 // }
-let robby = new Robot()
-console.log(robby.legs);
+//
+// let robby = new Robot()
+// console.log(Robot.__proto__);
+
+
+function Robot() {
+}
+Robot.prototype.parts=[];
+Robot.prototype.capabilities=[];
+
+let robby = new Robot();
+let cranky = new Robot();
+
+robby.parts=['core'];
+Robot.prototype.capabilities.push('fly');
+
+
+console.log(cranky.parts);
